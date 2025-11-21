12658f918822fe1e8a539267dec8a8d905c2017e
vitormelo260 <vitormelo260@usp.br>
2025-11-21 15:50:38 -0300

    Update blinkled_rpigpio.py

--- blinkled_rpigpio.py
+++ blinkled_rpigpio.py
@@ -1,15 +1,16 @@
 #!/home/sel/VitorJoao/bin/python3
-import RPi.GPIO as GPIO
-from time import sleep
+#Indica o interpretador que deve ser utilizado (Python 3) e seu diretório. 
+import RPi.GPIO as GPIO #Importa a biblioteca RPi.GPIO como GPIO 
+from time import sleep #Importa a função sleep da biblioteca time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(17,GPIO.OUT)
+GPIO.setmode(GPIO.BCM) #Define os pinos da placa de acordo com o BCM e não com a numeração física.
+GPIO.setup(18,GPIO.OUT) #Define o GPIO18 como output
+GPIO.setup(17,GPIO.OUT) #Define o GPIO17 como output
 while True:
-	GPIO.output(17,False)
-	GPIO.output(18,True)
-	sleep(1)
-	print("Ligado")
-	GPIO.output(18,False)
-	sleep(1)
-	print("Desligado")
+	GPIO.output(17,False) #Atribui nivel lógico baixo para o GPIO17
+	GPIO.output(18,True) #Atribui nivel lógico alto para o GPIO18
+	sleep(1) #Pausa por 1 segundo
+	print("Ligado") #Printa ligado no terminal
+	GPIO.output(18,False) #Atribui nivel lógico baixo para o GPIO18
+	sleep(1) #Pausa por 1 segundo
+	print("Desligado") #Printa desligado no terminal
